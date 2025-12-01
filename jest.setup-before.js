5f15064b1891acaf639e78284acb31941e772721
Onaje Oladuwa <ooladuwa@gmail.com>
2025-12-01 10:17:16 -0500

    fix: CI lint errors

--- jest.setup-before.js
+++ jest.setup-before.js
@@ -1,5 +1,6 @@
 /* eslint-env jest, node */
 /* global global */
+import { jest } from '@jest/globals';
 
 // jest.setup-before.js - Runs BEFORE Jest loads
 jest.mock('expo/src/winter/runtime.native', () => ({}), { virtual: true });
